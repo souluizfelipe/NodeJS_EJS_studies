f18bd36561c6a060f2ba33d197f84382ca2de2a8
Luiz Felipe Pereira <30962248+souluizfelipe@users.noreply.github.com>
2021-06-06 16:25:19 -0300

    create about page and organized the views folder

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (rec, res) => {
-    res.render('index');
+    res.render('pages/index');
+});
+
+app.get('/sobre', (rec, res) => {
+    res.render('pages/about');
 });
 
 app.listen(9999);
-console.log('working');
+console.log('working...');
